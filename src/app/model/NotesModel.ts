9a3c4ec4f53acbd4dec93b7ef71767ca85453e42
PS <pooja.singh@pristinebs.co.in>
2022-09-03 18:29:26 +0530

    d

---
+++ src/app/model/NotesModel.ts
@@ -0,0 +1,13 @@
+export class notesModel{
+    attachment_1?: string;
+    attachment_2?: string;
+    condition: string;
+    created_by: string;
+    created_on: string;
+    lead_code: string;
+    note_id: string;
+    note_title: string;
+    remarks: string;
+    updated_by: string;
+    updated_on: string;
+}
